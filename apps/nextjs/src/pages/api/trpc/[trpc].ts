06ec6f4ff16ce60065c23670402fe9c3ac4b346e
Daniel <70210356+Itsnotaka@users.noreply.github.com>
2023-07-16 22:32:19 +0800

    .

--- apps/nextjs/src/pages/api/trpc/[trpc].ts
+++ apps/nextjs/src/pages/api/trpc/[trpc].ts
@@ -1,4 +1,16 @@
 import { createNextApiHandler } from "@acme/trpc/server/createNextApiHandler";
 import { publicViewerRouter } from "@acme/trpc/server/routers/publicViewer/_router";
 
-export default createNextApiHandler(publicViewerRouter);
+import { type NextApiRequest, type NextApiResponse } from "next";
+import cors from "nextjs-cors";
+
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  // Enable cors
+  await cors(req, res);
+
+  // Let the tRPC handler do its magic
+  return await createNextApiHandler(publicViewerRouter)(req, res);
+};
+
+export default handler;
