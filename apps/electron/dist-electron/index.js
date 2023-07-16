4ebcd04b5a1c5f3b6e82beae7e1e3ebcafabd7f1
Daniel <70210356+Itsnotaka@users.noreply.github.com>
2023-07-16 22:28:58 +0800

    [ci] debugging electron

---
+++ apps/electron/dist-electron/index.js
@@ -0,0 +1,19 @@
+"use strict";
+const path = require("path");
+const electron = require("electron");
+const dist = path.join(__dirname, "../dist");
+const preload = path.join(__dirname, "./preload.js");
+const url = process.env.VITE_DEV_SERVER_URL;
+electron.app.on("ready", () => {
+  const win = new electron.BrowserWindow({
+    webPreferences: {
+      preload
+    }
+  });
+  if (url) {
+    void win.loadURL(url);
+  } else {
+    void win.loadFile(path.join(dist, "index.html"));
+  }
+  win.show();
+});
---
+++ apps/electron/src/index.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { httpBatchLink } from "@trpc/client";
+import { createTRPCReact } from "@trpc/react-query";
+import { createRoot } from "react-dom/client";
+import superjson from "superjson";
+
+import { type AppRouter } from "@acme/trpc/server/routers/_app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const trpc = createTRPCReact<AppRouter>();
+
+function App() {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      transformer: superjson,
+      links: [
+        httpBatchLink({
+          url: "http://localhost:3000/api/trpc",
+        }),
+      ],
+    }),
+  );
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <HelloElectron />
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
+}
+
+function HelloElectron() {
+  const { data } = trpc.viewer.public.i18n.useQuery();
+
+  if (!data) return null;
+
+  return (
+    <div>
+      <h1>Hello Electron!</h1>
+      {data.i18n` `}
+    </div>
+  );
+}
+
+// craete root
+const container = document.getElementById("react-root");
+if (!container) throw new Error("No container");
+createRoot(container).render(<App />);
